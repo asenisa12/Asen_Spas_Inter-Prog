04be05056077b24d31aadf0b3ea3885447159c46
asenisa12 <asenisa@abv.bg>
2014-12-30 17:40:21 +0200

    task 14-20

--- Holiday_JS_HW/WebContent/scripts/selection.js
+++ Holiday_JS_HW/WebContent/scripts/selection.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	"use strict"
-	
+	var JSONPlaceholder = "http://jsonplaceholder.typicode.com/posts";
 	//2)
 	console.log($("#footer a:first"));
 	
@@ -53,26 +53,31 @@ $(document).ready(function() {
 	
 	function appendToList(list, post){
 		var newElement = $("<li/>");
-		newElement.text(post.title);
+		newElement.text(post);
 		list.append(newElement);
+		return newElement;
 	}
 	function processResponse(response){
 		var list = $("#posts");
 		var i=0;
 		$.each(response, function(){
-			appendToList(list, this);
+			appendToList(list, this.title);
 			if(i++ >=5){
 				return false;
 			}
 		});
 	}
 	
-	$.ajax("http://jsonplaceholder.typicode.com/posts", {
+	$.ajax(JSONPlaceholder, {
 		  method: "GET"
 	}).then(processResponse, handleError);
 	
 	//12)
 	var postnum =1;
+	function deleteElem(elem){
+		
+	}
+	
 	var addElement = function(){
 		var text = inputText.val();
 		if(!text) {
@@ -80,18 +85,62 @@ $(document).ready(function() {
 			return;
 		}
 	//13)
-		$.ajax("http://jsonplaceholder.typicode.com/posts", {
-			method: "Post",
+		$.ajax(JSONPlaceholder, {
+			method: "POST",
 			data: {
 				title: 'Title '+postnum,
 				body: text,
 				userId: postnum
 			}
+		}).then(function(data){
+	//14)
+			$.getJSON(JSONPlaceholder+"/"+data.id, function(json){
+				var list = appendToList($("#posts"),json.id+" "+json.body);
+				list.addClass("akk")
+	//15)		
+				var delbutton = list.append("<button>X</button>");
+				delbutton.click(function(){
+					alert("DELETE!!!");
+	//16)			
+					console.log(list);
+					if(confirm("delete element?")){
+						$.ajax({
+							url: JSONPlaceholder+"/"+json.id,
+							type: "DELETE",
+							data: json,
+							success:  list.remove()
+						});
+					}
+				});
+			});
 		});
 		postnum++;
 		inputText.val("");
-	};
-	
-	
+	};	
 	button.click(addElement);
+	
+	//17)
+	$("#posts").prepend("<input id = 'textinput2'/>");
+	var input = $("#textinput2");
+	//18)
+	input.change(function(){
+		var list;
+		$.ajax("http://jsonplaceholder.typicode.com/posts?userId="+input.val(), {
+			method: "GET"
+		}).then(function(response){
+	//19)
+		$("#posts .akk").remove();	
+	//20)
+			list = appendToList($("#posts"),response);
+			var delbutton = list.append("<button>X</button>");
+			delbutton.click(function(){
+				alert("DELETE!!!");
+				if(confirm("delete element?")){
+					list.remove();
+				}
+			});	
+		});
+		input.val("");
+	});
+	
 });
